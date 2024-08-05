7323be5d3d2fd29d7db2d29654e94cb75d7b9368
[seung0205] <[seunghyuni00@naver.com]>
2024-08-06 06:43:14 +0900

    Character Picking up the Item & Filling the bar

---
+++ Assets/Scripts/UI Scripts/InventorySlot_UI.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class InventorySlot_UI : MonoBehaviour
+{
+    [SerializeField] private Image itemSprite;
+    [SerializeField] private TextMeshProUGUI itemCount;
+    [SerializeField] private InventorySlot assignedInventorySlot;
+
+    private Button button;
+
+    public InventorySlot AssignedInventorySlot => assignedInventorySlot;
+    public InventoryDisplay ParentDisplay {get; private set;}
+
+    private void Awake()
+    {
+        ClearSlot();
+
+        button = GetComponent<Button>();
+        button?.onClick.AddListener(OnUISlotClick);
+
+        ParentDisplay = transform.parent.GetComponent<InventoryDisplay>();
+    }
+
+    public void Init(InventorySlot slot)
+    {
+        assignedInventorySlot = slot;
+        UpdateUISlot(slot);
+    }
+
+    public void UpdateUISlot(InventorySlot slot)
+    {
+        if(slot.ItemData != null)
+        {
+            itemSprite.sprite = slot.ItemData.Icon;
+            itemSprite.color = Color.white;
+
+            if (slot.StackSize > 1) itemCount.text = slot.StackSize.ToString();
+            else itemCount.text = ""; //item 갯수가 1이면 따로 표시하지 않음
+        }
+        else{
+            ClearSlot();
+        }
+
+        
+    }
+
+    public void UpdateUISlot()
+    {
+        if (assignedInventorySlot != null) UpdateUISlot(assignedInventorySlot);
+    }
+
+    public void ClearSlot()
+    {
+        assignedInventorySlot?.ClearSlot();
+        itemSprite.sprite = null;
+        itemSprite.color = Color.clear;
+        itemCount.text = "";
+    }
+    public void OnUISlotClick()
+    {
+        ParentDisplay?.SlotClicked(this);
+    }
+}
