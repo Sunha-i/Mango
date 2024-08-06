08a9ca86a69628526f4809bc51a02132f9cb68f2
[seung0205] <[seunghyuni00@naver.com]>
2024-08-07 03:20:56 +0900

    Clicking Itmes and Putting it Into the Bar

--- Assets/Scripts/UI Scripts/InventoryDisplay.cs
+++ Assets/Scripts/UI Scripts/InventoryDisplay.cs
@@ -29,9 +29,32 @@ public abstract class InventoryDisplay : MonoBehaviour
         }
     }
 
-    public void SlotClicked(InventorySlot_UI clickedSlot)
+    public void SlotClicked(InventorySlot_UI clickedUISlot)
     {
-        Debug.Log("Slot Clicked");
+        
+        // Clicked slot has an item - mouse doesn't have an item - pick up that item.
+
+        if (clickedUISlot.AssignedInventorySlot.ItemData != null && mouseInventoryItem.AssignedInventorySlot.ItemData == null)
+        {
+            // If player is holding shift key? Split the stack.
+
+            mouseInventoryItem.UpdateMouseSlot(clickedUISlot.AssignedInventorySlot);
+            clickedUISlot.ClearSlot();
+            return;
+        }
+
+        //Clicked slot doesn't have an item - Mouse does have an item - place the mouse item into the empty slot.
+        if (clickedUISlot.AssignedInventorySlot.ItemData == null && mouseInventoryItem.AssignedInventorySlot.ItemData != null)
+        {
+            clickedUISlot.AssignedInventorySlot.AssignItem(mouseInventoryItem.AssignedInventorySlot);
+            clickedUISlot.UpdateUISlot();
+
+            mouseInventoryItem.ClearSlot();
+        }
+        //Both slots have an item - decide what to do...
+            //Are both items the same? If so combine them.
+                // If the slot stack size + mouse stack size > the slot Max Stack Size? If so, take from mouse.
+            //If different items, then swap the items.
     }
 
 }
